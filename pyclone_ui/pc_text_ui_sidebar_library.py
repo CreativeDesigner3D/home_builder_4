1f75452dba88dbcd1884fe94fe31d7f4be8794c7
Andrew Peel <34818078+CreativeDesigner3D@users.noreply.github.com>
2024-02-15 16:21:49 -0800

    Adding text side bar panel to view current library code.

--- pyclone_ui/pc_text_ui_sidebar_library.py
+++ pyclone_ui/pc_text_ui_sidebar_library.py
@@ -14,7 +14,9 @@ from bpy.props import (
         CollectionProperty,
         )
 import os
+import subprocess
 from .. import pyclone_utils
+from .. import hb_utils
 from pc_lib import pc_utils, pc_types
 
 class TEXT_PT_python_crash_course(Panel):
@@ -42,13 +44,71 @@ class TEXT_PT_pc_examples(Panel):
         layout = self.layout    
         # layout.label(text="PyClone Examples")
         TEMPLATE_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)),'docs','python_templates','PyClone Examples')
-        layout.operator('text.open',text="Assembly Creation").filepath = os.path.join(TEMPLATE_PATH,"assembly_creation.py")
-        layout.operator('text.open',text="Assembly Export").filepath = os.path.join(TEMPLATE_PATH,"assembly_export.py")
-        layout.operator('text.open',text="Home Builder Automation").filepath = os.path.join(TEMPLATE_PATH,"home_builder_automation.py")
+        files = os.listdir(TEMPLATE_PATH)
+        for f in files:
+            file_name, ext = os.path.splitext(f)
+            if '.py' in ext:
+                layout.operator('text.open',text=file_name).filepath = os.path.join(TEMPLATE_PATH,f)        
 
+
+class TEXT_PT_home_builder_libraries(Panel):
+    bl_space_type = "TEXT_EDITOR"
+    bl_region_type = "UI"
+    bl_category = "Libraries"
+    bl_label = "Home Builder Libraries"
+    bl_options = {'DEFAULT_CLOSED'}
+
+    def draw(self, context):
+        layout = self.layout
+        wm_props = context.window_manager
+        library = hb_utils.get_active_library(context)
+        library_root_folder = os.path.dirname(os.path.dirname(library.library_path))
+        library_init_file = os.path.join(library_root_folder,"__init__.py")
+        if library:
+            layout.operator('pc_text.open_blend_file',text=library.name + " " + "Thumbnails").filepath = os.path.join(library.library_path,"library.blend")
+            layout.operator('text.open',text=library.name).filepath = library_init_file
+            files = os.listdir(library_root_folder)
+            init_file_box = layout.box()
+            operator_box = layout.box()
+            operator_box.label(text="Operator Modules")
+            data_box = layout.box()
+            data_box.label(text="Data Modules")                
+            property_box = layout.box()
+            property_box.label(text="Property Modules")
+            ui_box = layout.box()
+            ui_box.label(text="UI Modules")            
+            for f in files:
+                file_name, ext = os.path.splitext(f)
+                if ext == '.py':
+                    if 'props_' in f:
+                        property_box.operator('text.open',text=file_name).filepath = os.path.join(library_root_folder,f)                    
+                    elif 'ops_' in f:
+                        operator_box.operator('text.open',text=file_name).filepath = os.path.join(library_root_folder,f)
+                    elif 'ui_' in f:
+                        ui_box.operator('text.open',text=file_name).filepath = os.path.join(library_root_folder,f)
+                    elif 'types_' in f:
+                        data_box.operator('text.open',text=file_name).filepath = os.path.join(library_root_folder,f)
+                    else:
+                        layout.operator('text.open',text=file_name).filepath = os.path.join(library_root_folder,f)
+                    
+
+class pc_text_OT_open_blend_file(bpy.types.Operator):
+    bl_idname = "pc_text.open_blend_file"
+    bl_label = "Open Blend File"
+    bl_description = "This exports the XML file for a room"
+
+    filepath: bpy.props.StringProperty()
+
+    def execute(self, context):
+        command = [bpy.app.binary_path,self.filepath]
+        subprocess.Popen(command)         
+        return {'FINISHED'}
+    
 classes = (
     TEXT_PT_python_crash_course,
     TEXT_PT_pc_examples,
+    TEXT_PT_home_builder_libraries,
+    pc_text_OT_open_blend_file,
 )
 
 register, unregister = bpy.utils.register_classes_factory(classes)
